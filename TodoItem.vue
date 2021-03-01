b6eaf660097e0fa106f11179dcb7659b5e282ee1
ydrea <55543450+ydrea@users.noreply.github.com>
2021-03-01 10:25:13 +0100

    Add files via upload
    traversing arrays

---
+++ TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div> 
+    <div> 
+ <h4> {{todo.title}}</h4>
+    </div> 
+</div> 
+
+    </template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"]
+}
+</script>
+
+<style scoped>
+
+</style>
