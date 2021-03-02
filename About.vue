5e867d7062b669fcc9226eb9630c7dc7fdf41030
ydrea <55543450+ydrea@users.noreply.github.com>
2021-03-02 16:34:30 +0100

    Add files via upload

---
+++ About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat ratione consequatur suscipit fugiat veritatis deserunt earum ut nostrum! Ipsa placeat repudiandae quaerat asperiores temporibus eligendi deserunt fugit similique qui id.
+  </div>
+</template>
